3ff1b4b2d38128ae77ee30db907bbd7750015c0e
Joseph <sejokarizz@gmail.com>
2023-05-19 21:53:31 +0300

    fix trailing words error  and add comments

--- split_line.c
+++ split_line.c
@@ -1,11 +1,31 @@
-#include <stdlib.h>
-
+#include "shell.h"
 #define TOK_DELIM " \t\r\n\a"
+
 /**
- * split_line
+ * resizeTokens - Resizes the tokens array if necessary.
+ * @tokens: Pointer to the pointer that holds the array of tokens.
+ * @bufsize: Pointer to the variable that holds the current buffer size.
+ * @i: The index being checked.
+ */
+
+void resizeTokens(char ***tokens, int *bufsize, int i)
+{
+	if (i >= *bufsize)
+	{
+		*bufsize += *bufsize;
+		*tokens = realloc(*tokens, (*bufsize) * sizeof(char *));
+		if (!(*tokens))
+			exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * split_line - Splits a string into tokens.
+ * @line: The string to be split.
  *
- * Return:
-*/
+ * Return: An array of tokens.
+ */
+
 char **split_line(char *line)
 {
 	int bufsize = 64;
@@ -13,31 +33,33 @@ char **split_line(char *line)
 	char **tokens = malloc(bufsize * sizeof(char *));
 	char *token, *saveptr;
 
-	if (!tokens) exit(EXIT_FAILURE);
+	if (!tokens)
+		exit(EXIT_FAILURE);
 
 	while (*line != '\0')
 	{
-	while (*line == ' ' || *line == '\t' || *line == '\r' ||
-		*line == '\n' || *line == '\a')   line++;
+		while (*line == ' ' || *line == '\t' || *line == '\r' ||
+		       *line == '\n' || *line == '\a')
+			line++;
 
-	if (*line == '#' || *line == '\0')  break;
+		if (*line == '#' || *line == '\0')
+		{
+			break;
+		}
 
-	token = line;
-	while (*line != ' ' && *line != '\t' && *line != '\r' &&
-		*line != '\n' && *line != '\a' && *line != '#' &&
-		*line != '\0') line++;
+		token = line;
+		while (*line != ' ' && *line != '\t' && *line != '\r' &&
+		       *line != '\n' && *line != '\a' && *line != '#' &&
+		       *line != '\0')
+			line++;
 
-	if (*line != '\0') *line++ = '\0';
+		if (*line != '\0')
+			*line++ = '\0';
 
-	tokens[i] = token;
-	i++;
+		tokens[i] = token;
+		i++;
 
-	if (i >= bufsize)
-	{
-		bufsize += bufsize;
-		tokens = realloc(tokens, bufsize * sizeof(char *));
-		if (!tokens) exit(EXIT_FAILURE);
-	}
+		resizeTokens(&tokens, &bufsize, i);
 	}
 
 	tokens[i] = NULL;
