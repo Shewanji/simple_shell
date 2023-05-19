aa0a73953c24b0b1d742a8ff52076f159cdcea99
Shewanji <marrionwanjiru26@gmail.com>
2023-05-19 11:45:57 -0700

    fix betty errors

--- split_line.c
+++ split_line.c
@@ -3,43 +3,43 @@
 #define TOK_DELIM " \t\r\n\a"
 /**
  * split_line
- * 
+ *
  * Return:
 */
 char **split_line(char *line)
 {
-    int bufsize = 64;
-    int i = 0;
-    char **tokens = malloc(bufsize * sizeof(char *));
-    char *token, *saveptr;
+	int bufsize = 64;
+	int i = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token, *saveptr;
 
-    if (!tokens) exit(EXIT_FAILURE);
+	if (!tokens) exit(EXIT_FAILURE);
 
-    while (*line != '\0')
-    {
-        while (*line == ' ' || *line == '\t' || *line == '\r' ||
-               *line == '\n' || *line == '\a')   line++;
+	while (*line != '\0')
+	{
+	while (*line == ' ' || *line == '\t' || *line == '\r' ||
+		*line == '\n' || *line == '\a')   line++;
 
-        if (*line == '#' || *line == '\0')  break;
+	if (*line == '#' || *line == '\0')  break;
 
-        token = line;
-        while (*line != ' ' && *line != '\t' && *line != '\r' &&
-               *line != '\n' && *line != '\a' && *line != '#' &&
-               *line != '\0') line++; 
+	token = line;
+	while (*line != ' ' && *line != '\t' && *line != '\r' &&
+		*line != '\n' && *line != '\a' && *line != '#' &&
+		*line != '\0') line++;
 
-        if (*line != '\0') *line++ = '\0';
+	if (*line != '\0') *line++ = '\0';
 
-        tokens[i] = token;
-        i++;
+	tokens[i] = token;
+	i++;
 
-        if (i >= bufsize)
-        {
-            bufsize += bufsize;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens) exit(EXIT_FAILURE);
-        }
-    }
+	if (i >= bufsize)
+	{
+		bufsize += bufsize;
+		tokens = realloc(tokens, bufsize * sizeof(char *));
+		if (!tokens) exit(EXIT_FAILURE);
+	}
+	}
 
-    tokens[i] = NULL;
-    return (tokens);
+	tokens[i] = NULL;
+	return (tokens);
 }
