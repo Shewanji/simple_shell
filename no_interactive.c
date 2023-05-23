52d25f4965483af9475cad52ebc5e62ee3899313
Joseph <sejokarizz@gmail.com>
2023-05-23 20:48:26 +0300

    update no intereactive shell code

--- no_interactive.c
+++ no_interactive.c
@@ -1,6 +1,28 @@
 #include "shell.h"
 
+/**
+ * no_interactive - Unix command line 4 non-interactive shell
+ *
+ * Return: void
+ */
 void no_interactive(void)
 {
-	printf("not yet done\n");
+	char *line;
+	char **args;
+	int status = -1;
+
+	do {
+		line = read_stream();
+		args = split_line(line);
+
+		status = execute_args(args);
+
+		free(line);
+		free(args);
+
+		if (status >= 0)
+		{
+			exit(status);
+		}
+	} while (status == -1);
 }
